b44b0e7cf699a246cff4c4e40ed9704f2cd00a31
aabdev <aabzel@yandex.ru>
2026-04-12 16:34:12 +0300

    + fix builds

---
+++ source/projects/jz_f407vet6_mbr_gcc_m/clean_temp.bat
@@ -0,0 +1,20 @@
+@echo off
+echo %0
+
+del /S *.o
+del /S *.obj
+del /S *.d
+del /S *.map
+del /S *.hex
+del /S *.elf
+del /S *.pdf
+del /S *.gv
+del /S *.jpeg
+del /S *.svg
+del /S *.lst
+del /S *.bin
+del /S *.su
+del /S *.pp
+del /S *.bak
+
+
