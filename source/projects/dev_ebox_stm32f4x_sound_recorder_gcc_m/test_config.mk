99542cbacd949dcfe2948325eb1cd504ab30028b
Барунин Александр Анатольевич <barunin@camozzi.ru>
2026-07-06 09:49:53 +0300

    + sound rec project

---
+++ source/projects/dev_ebox_stm32f4x_sound_recorder_gcc_m/test_config.mk
@@ -0,0 +1,97 @@
+ifneq ($(TEST_CONFIG_INC),Y)
+    TEST_CONFIG_INC=Y
+
+
+    TEST_MEMORY=Y
+
+    ifeq ($(IR_RECEIVER),Y)
+        TEST_IR_RECEIVER=Y
+    endif
+    
+    ifeq ($(SUPER_CYCLE),Y)
+        TEST_SUPER_CYCLE=Y
+    endif
+
+    ifeq ($(NVIC),Y)
+        TEST_NVIC=Y
+    endif
+
+    ifeq ($(UART),Y)
+        TEST_UART=Y
+    endif
+
+    ifeq ($(SOUND_RECORDER),Y)
+        TEST_SOUND_RECORDER=Y
+    endif
+
+    ifeq ($(I2C),Y)
+        TEST_I2C=Y
+    endif
+
+    ifeq ($(FILE_MCAL),Y)
+        TEST_FILE_MCAL=Y
+    endif
+
+    ifeq ($(DMA_CHANNEL),Y)
+        TEST_DMA_CHANNEL=Y
+    endif
+
+    ifeq ($(DDS),Y)
+        TEST_DDS=Y
+    endif
+
+    ifeq ($(I2S),Y)
+        TEST_I2S=Y
+    endif
+
+    ifeq ($(EXT_INT),Y)
+        TEST_EXT_INT=Y
+    endif
+
+    ifeq ($(WM8731),Y)
+        TEST_WM8731=Y
+    endif
+
+    ifeq ($(CSV),Y)
+        TEST_CSV=Y
+    endif
+    
+    ifeq ($(WAV),Y)
+        TEST_WAV=Y
+    endif
+
+    ifeq ($(SYSTICK),Y)
+        TEST_SYSTICK=Y
+    endif
+
+    ifeq ($(SDIO),Y)
+        TEST_SDIO=Y
+    endif
+
+    ifeq ($(FAT_FS),Y)
+        TEST_FAT_FS=Y
+    endif
+
+    ifeq ($(TIME),Y)
+        TES_TIMER=Y
+    endif
+    
+    ifeq ($(SOFTWARE_TIMER),Y)
+        TEST_SOFTWARE_TIMER=Y
+    endif
+
+    ifeq ($(TIMER),Y)
+        TEST_TIMER=Y
+    endif
+    
+    ifeq ($(COMPUTING),Y)
+        TEST_COMPUTING=Y
+    endif
+
+    ifeq ($(UNIT_TEST),Y)
+        TEST_SW=Y
+        TEST_HW=Y
+        MCAL_OPT = -DHAS_TEST
+        TEST=Y
+    endif
+endif
---
+++ source/projects/dev_ebox_stm32f4x_sound_recorder_gcc_m/version.h
@@ -0,0 +1,8 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION_GENERATION 1
+#define CONFIG_NAME "SoundRecorder"
+#define FW_NAME "Ba-43"
+
+#endif /* VERSION_H */
